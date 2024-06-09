dc24ef6ff042305185100395d83943526ee95fc2
balankdharan <balankdharan@gmail.com>
2024-06-09 23:16:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import Navigation from "./pages/navigation/Navigation";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
+
+export default App;
