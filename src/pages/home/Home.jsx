474ea2e76662f854dc61587d89d5adb2bae87798
balankdharan <balankdharan@gmail.com>
2024-06-20 19:41:03 +0530

    Ui

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,29 @@
+import { motion } from "framer-motion";
 import HomePage from "../../components/HomePage";
 import SecondContent from "../../components/SecondContent";
 import ThirdSection from "../../components/ThirdSection";
 
 const Home = () => {
+  const slideRightVariants = {
+    hidden: { opacity: 0, x: -200 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.6,
+      },
+    },
+  };
+
   return (
     <>
-      <HomePage />
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={slideRightVariants}
+      >
+        <HomePage />
+      </motion.div>
       <SecondContent />
       <ThirdSection />
     </>
